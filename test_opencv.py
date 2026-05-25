c5f1407de690aa2698cc24dcedbe21007168abdb
Chon <bonuz@gmail.com>
2026-05-25 20:12:00 +0700

    add rectangle/dot

--- test_opencv.py
+++ test_opencv.py
@@ -3,6 +3,8 @@ import cv2
 img = cv2.imread('images/ZiJZu.png')
 resizeimg = cv2.resize(img, (600,400))
 cv2.arrowedLine(resizeimg, (0,0), (300,200), (255,0,0), 4)
+cv2.rectangle(resizeimg, (200,100), (500,400), (0,0,255), 5)
+cv2.circle(resizeimg, (300,200), 10, (0,255,0), -1)
 cv2.imshow('Image', resizeimg)
 
 cv2.waitKey(0)
